433cf433e0b7972c7e169783f824e75e5d3e7f9b
notsolvablehat <notsolvablehat@gmail.com>
2026-07-22 13:21:14 +0530

    init: dependencies and linter config

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+
+const eslintConfig = defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "off",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  globalIgnores(["dist", "build", "node_modules"]),
+])
+
+export default eslintConfig
